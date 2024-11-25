0bf446df2520d304659fee263780b7b388495e20
thomas <info@englishreservoir.com>
2024-11-25 16:57:56 +0100

    testing new foreach loop

--- includes/nav.php
+++ includes/nav.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 
-require_once '../functions.php';
+require_once '/functions.php';
 $nav_data = load_json('/home/thomas/englishreservoir.es/data/includes/nav.json');
 
 // Check if the JSON data was successfully loaded
