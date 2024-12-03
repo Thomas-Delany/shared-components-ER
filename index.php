4407c980828afaacd39c0dfaea9a7f267a7c48cc
Thomas <info@englishreservoir.com>
2024-12-03 21:00:23 +0100

    new file paths for config.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/includes/functions.php';
-$index_data = load_json(CENTRAL_PATH . '../data/pages/index.json');
+require_once CENTRAL_PATH . 'includes/functions.php';
+$index_data = load_json(CENTRAL_PATH . 'data/pages/index.json');
 
 // JSON data includes
 $indexData = $index_data['sections'];
