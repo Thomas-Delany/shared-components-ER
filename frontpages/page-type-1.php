626426e68aa7d09bd032e79d5f14b924369692be
thomas <info@englishreservoir.com>
2024-12-10 17:30:00 +0100

    section2 check

--- frontpages/page-type-1.php
+++ frontpages/page-type-1.php
@@ -104,69 +104,66 @@ $section1 = array_filter($pageSections, fn($section) => $section['type'] === 'Se
 
 
                 <!-- Steps START -->
-                <div class="row g-lg-5 mt-3">
-                    <!-- Item -->
-                    <div class="col-sm-4 col-lg-4 text-center position-relative">
-                        <!-- SVG decoration -->
-                        <figure class="position-absolute top-0 start-100 translate-middle d-none d-lg-block">
-                            <svg width="182.9px" height="86px" viewBox="0 0 182.9 86">
-                                <path class="fill-secondary"
-                                    d="M127.3,19.8c0.9,0.7,1.8,1.5,2.8,2c9.3,4.5,17.1,11.1,24.4,18.3c6.5,6.4,11.9,13.7,15.8,22 c1.5,3.2,2.7,6.6,4.2,10.2c2.5-4.2,4.1-8.9,8.6-11.5c-2.2,3.9-4.7,7.7-6.5,11.9c-1.7,4.1-2.6,8.6-3.9,13.4 c-4.1-6.1-7-13.2-14.9-15.6c3.8-1.4,6.2,0.5,14.1,10.3c1-2.2,1.8-4.1,1.1-6.5c-3.8-13.6-11.4-24.8-21.4-34.6 c-5.8-5.7-12.3-10.6-19.2-14.9c-7-4.3-14.4-8-22.2-10.9c-10.7-3.8-21.5-6.6-32.8-7.7C63.9,5,50.7,5.9,38,10.4 c-14.1,5-26,13.2-35,25.4c-0.5,0.7-1.2,1.4-1.8,2.1C1.1,38,0.8,38,0.4,38.1c-0.9-0.9-0.2-1.7,0.3-2.4c4.7-6.7,10.5-12.4,17.2-17.1 C31.7,8.9,47.2,4.7,63.8,4C77.1,3.5,90,5.8,102.7,9.3c2.2,0.6,4.3,1.8,6.5,2.6c0.9,0.4,2,0.5,2.9,0.7 C117.1,15,122.2,17.4,127.3,19.8z" />
-                            </svg>
-                        </figure>
+                <?php foreach ($section2 as $data): ?>
+                    <div class="row g-lg-5 mt-3">
+                        <!-- Item -->
+                        <div class="col-sm-4 col-lg-4 text-center position-relative">
+                            <!-- SVG decoration -->
+                            <figure class="position-absolute top-0 start-100 translate-middle d-none d-lg-block">
+                                <svg width="182.9px" height="86px" viewBox="0 0 182.9 86">
+                                    <path class="fill-secondary"
+                                        d="M127.3,19.8c0.9,0.7,1.8,1.5,2.8,2c9.3,4.5,17.1,11.1,24.4,18.3c6.5,6.4,11.9,13.7,15.8,22 c1.5,3.2,2.7,6.6,4.2,10.2c2.5-4.2,4.1-8.9,8.6-11.5c-2.2,3.9-4.7,7.7-6.5,11.9c-1.7,4.1-2.6,8.6-3.9,13.4 c-4.1-6.1-7-13.2-14.9-15.6c3.8-1.4,6.2,0.5,14.1,10.3c1-2.2,1.8-4.1,1.1-6.5c-3.8-13.6-11.4-24.8-21.4-34.6 c-5.8-5.7-12.3-10.6-19.2-14.9c-7-4.3-14.4-8-22.2-10.9c-10.7-3.8-21.5-6.6-32.8-7.7C63.9,5,50.7,5.9,38,10.4 c-14.1,5-26,13.2-35,25.4c-0.5,0.7-1.2,1.4-1.8,2.1C1.1,38,0.8,38,0.4,38.1c-0.9-0.9-0.2-1.7,0.3-2.4c4.7-6.7,10.5-12.4,17.2-17.1 C31.7,8.9,47.2,4.7,63.8,4C77.1,3.5,90,5.8,102.7,9.3c2.2,0.6,4.3,1.8,6.5,2.6c0.9,0.4,2,0.5,2.9,0.7 C117.1,15,122.2,17.4,127.3,19.8z" />
+                                </svg>
+                            </figure>
 
-                        <div class="px-4">
-                            <!-- Image -->
-                            <div class="icon-xxl bg-body shadow mx-auto rounded-circle mb-3">
-                                <img src="<?php echo BASE_URL; ?>assets/images/element/26.svg" alt="">
+                            <div class="px-4">
+                                <!-- Image -->
+                                <div class="icon-xxl bg-body shadow mx-auto rounded-circle mb-3">
+                                    <img src="<?php echo BASE_URL; ?>assets/images/element/26.svg" alt="">
+                                </div>
+                                <!-- Title -->
+                                <h3><?= htmlspecialchars($data['titles'][0]) ?></h3>
+                                <p class="lead"><?= htmlspecialchars($data['paragraphs'][0]) ?>">
+                                </p>
                             </div>
-                            <!-- Title -->
-                            <h3>Method</h3>
-                            <p class="lead">Everything is taught in an especially inductive manner with context and
-                                dialogue whilst implementing our method of instruction: explanation — context —
-                                analysis.
-                            </p>
+                        </div>
+
+                        <!-- Item -->
+                        <div class="col-sm-4 col-lg-4 text-center pt-0 pt-lg-5 position-relative">
+                            <!-- SVG decoration -->
+                            <figure class="position-absolute top-100 start-100 translate-middle mt-n3 d-none d-lg-block">
+                                <svg width="182.9px" height="86px" viewBox="0 0 182.9 86">
+                                    <path class="fill-secondary"
+                                        d="M127.3,70.2c0.9-0.7,1.8-1.5,2.8-2c9.3-4.5,17.1-11.1,24.4-18.3c6.5-6.4,11.9-13.7,15.8-22 c1.5-3.2,2.7-6.6,4.2-10.2c2.5,4.2,4.1,8.9,8.6,11.5c-2.2-3.9-4.7-7.7-6.5-11.9c-1.7-4.1-2.6-8.6-3.9-13.4 c-4.1,6.1-7,13.2-14.9,15.6c3.8,1.4,6.2-0.5,14.1-10.3c1,2.2,1.8,4.1,1.1,6.5c-3.8,13.6-11.4,24.8-21.4,34.6 c-5.8,5.7-12.3,10.6-19.2,14.9c-7,4.3-14.4,8-22.2,10.9c-10.7,3.8-21.5,6.6-32.8,7.7C63.9,85,50.7,84.1,38,79.6 c-14.1-5-26-13.2-35-25.4c-0.5-0.7-1.2-1.4-1.8-2.1c-0.1-0.1-0.4-0.1-0.8-0.2c-0.9,0.9-0.2,1.7,0.3,2.4c4.7,6.7,10.5,12.4,17.2,17.1 c13.7,9.7,29.2,14,45.9,14.6c13.3,0.5,26.2-1.8,38.8-5.3c2.2-0.6,4.3-1.8,6.5-2.6c0.9-0.4,2-0.5,2.9-0.7 C117.1,74.9,122.2,72.6,127.3,70.2z" />
+                                </svg>
+                            </figure>
+
+                            <div class="px-4">
+                                <!-- Image -->
+                                <div class="icon-xxl bg-body shadow mx-auto rounded-circle mb-3">
+                                    <img src="<?php echo BASE_URL; ?>assets/images/element/account.svg" alt="">
+                                </div>
+                                <!-- Title -->
+                                <h3><?= htmlspecialchars($data['titles'][1]) ?></h3>
+                                <p class="lead"><?= htmlspecialchars($data['paragraphs'][1]) ?>">
+                                </p>
+                            </div>
+                        </div>
+
+                        <!-- Item -->
+                        <div class="col-sm-4 col-lg-4 text-center">
+                            <div class="px-4">
+                                <!-- Image -->
+                                <div class="icon-xxl bg-body shadow mx-auto rounded-circle mb-3">
+                                    <img src="<?php echo BASE_URL; ?>assets/images/element/23.svg" alt="">
+                                </div>
+                                <!-- Title -->
+                                <h3><?= htmlspecialchars($data['titles'][2]) ?></h3>
+                                <p class="lead"><?= htmlspecialchars($data['paragraphs'][2]) ?>"></p>
+                            </div>
                         </div>
                     </div>
-
-                    <!-- Item -->
-                    <div class="col-sm-4 col-lg-4 text-center pt-0 pt-lg-5 position-relative">
-                        <!-- SVG decoration -->
-                        <figure class="position-absolute top-100 start-100 translate-middle mt-n3 d-none d-lg-block">
-                            <svg width="182.9px" height="86px" viewBox="0 0 182.9 86">
-                                <path class="fill-secondary"
-                                    d="M127.3,70.2c0.9-0.7,1.8-1.5,2.8-2c9.3-4.5,17.1-11.1,24.4-18.3c6.5-6.4,11.9-13.7,15.8-22 c1.5-3.2,2.7-6.6,4.2-10.2c2.5,4.2,4.1,8.9,8.6,11.5c-2.2-3.9-4.7-7.7-6.5-11.9c-1.7-4.1-2.6-8.6-3.9-13.4 c-4.1,6.1-7,13.2-14.9,15.6c3.8,1.4,6.2-0.5,14.1-10.3c1,2.2,1.8,4.1,1.1,6.5c-3.8,13.6-11.4,24.8-21.4,34.6 c-5.8,5.7-12.3,10.6-19.2,14.9c-7,4.3-14.4,8-22.2,10.9c-10.7,3.8-21.5,6.6-32.8,7.7C63.9,85,50.7,84.1,38,79.6 c-14.1-5-26-13.2-35-25.4c-0.5-0.7-1.2-1.4-1.8-2.1c-0.1-0.1-0.4-0.1-0.8-0.2c-0.9,0.9-0.2,1.7,0.3,2.4c4.7,6.7,10.5,12.4,17.2,17.1 c13.7,9.7,29.2,14,45.9,14.6c13.3,0.5,26.2-1.8,38.8-5.3c2.2-0.6,4.3-1.8,6.5-2.6c0.9-0.4,2-0.5,2.9-0.7 C117.1,74.9,122.2,72.6,127.3,70.2z" />
-                            </svg>
-                        </figure>
-
-                        <div class="px-4">
-                            <!-- Image -->
-                            <div class="icon-xxl bg-body shadow mx-auto rounded-circle mb-3">
-                                <img src="<?php echo BASE_URL; ?>assets/images/element/account.svg" alt="">
-                            </div>
-                            <!-- Title -->
-                            <h3>How does it work?</h3>
-                            <p class="lead">Each course is a compilation of videos with more than 3 hours of content.
-                                Each video lasts between 5 - 13 minutes. There is a Section Review after every lesson.
-                                These section reviews are multiple-choice.
-                            </p>
-                        </div>
-                    </div>
-
-                    <!-- Item -->
-                    <div class="col-sm-4 col-lg-4 text-center">
-                        <div class="px-4">
-                            <!-- Image -->
-                            <div class="icon-xxl bg-body shadow mx-auto rounded-circle mb-3">
-                                <img src="<?php echo BASE_URL; ?>assets/images/element/23.svg" alt="">
-                            </div>
-                            <!-- Title -->
-                            <h3>In a nutshell</h3>
-                            <p class="lead">Our English courses have more than 3 hours of content. Log in to stream your
-                                course online. Narrated in Spanish (the language of instruction is Spanish).</p>
-                        </div>
-                    </div>
-                </div>
+                <?php endforeach; ?>
                 <!-- Steps END -->
             </div>
         </section>
