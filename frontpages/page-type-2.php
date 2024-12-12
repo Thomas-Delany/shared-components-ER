80023d4684d38bdfa633bdff75430d365b682d57
thomas <info@englishreservoir.com>
2024-12-12 14:33:11 +0100

    json array structure changed

--- frontpages/page-type-2.php
+++ frontpages/page-type-2.php
@@ -229,9 +229,9 @@ Main Banner START -->
                         <div class="col-md-6 my-5">
                             <?php foreach ($data['titles'] as $title): ?>
                                 <h3 class="mb-3 mt-5"><?= htmlspecialchars($title['header']) ?></h3>
-                                <?php foreach ($title['paragraphs'] as $paragraph): ?>
-                                    <p class="lead"><?= htmlspecialchars($paragraph) ?></p>
-                                <?php endforeach; ?>
+
+                                <p class="lead"><?= htmlspecialchars($title['paragraphs']) ?></p>
+
                             <?php endforeach; ?>
                         </div>
                         <div class="col-md-5 d-flex justify-content-center align-items-center">
